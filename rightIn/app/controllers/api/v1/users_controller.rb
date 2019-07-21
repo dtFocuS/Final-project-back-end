0c92943febad5e206bd958fb3f9e1fb79e862cc4
Danny Tseng <dtfocus1025@gmail.com>
2019-07-20 19:35:26 -0700

    fixed syntax error

--- rightIn/app/controllers/api/v1/users_controller.rb
+++ rightIn/app/controllers/api/v1/users_controller.rb
@@ -28,6 +28,6 @@ class Api::V1::UsersController < ApplicationController
     private
  
     def user_params
-        params.require(:user).permit(:name, :username, :password, :image, :email)
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :image, :email)
     end
 end
