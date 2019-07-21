cc79efafe731e25e32dcbcb42ef64c58ee0c3185
Danny Tseng <dtfocus1025@gmail.com>
2019-07-20 17:03:24 -0700

    implemented jwt auth

---
+++ rightIn/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create, :index, :show]
+
+    def index
+        users = User.all
+        render json: users
+    end
+ 
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: UserSerializer.new(user)
+    end
+ 
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token({ user_id: @user.id })
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+ 
+    private
+ 
+    def user_params
+        params.require(:user).permit(:name, :username, :password, :image, :email)
+    end
+end
