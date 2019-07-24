962b2d77f40e193e2d061638b0ce54827f402dca
Danny Tseng <dtfocus1025@gmail.com>
2019-07-23 23:41:54 -0700

    built notification controller

--- rightIn/app/controllers/api/v1/participations_controller.rb
+++ rightIn/app/controllers/api/v1/participations_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::ParticipationsController < ApplicationController
         participations = Participation.all 
         render json: participations
     end
+
+    def create
+        participation = Participation.create(participation_params)
+        if participation.valid?
+            render json: { participation: ParticipationSerializer.new(participation) }, status: :created
+        else
+            render json: { error: 'failed to create participation' }, status: :not_acceptable
+        end
+    end
+ 
+    private
+ 
+    def participation_params
+        params.require(:participation).permit(:activity_id, :user_id)
+    end
 end
--- rightIn/app/models/user.rb
+++ rightIn/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
     has_many :activities
+    has_many :participations
+    accepts_nested_attributes_for :activities
 end
